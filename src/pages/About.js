016dca6561cb5ca9b25b325963900d548d12025b
Alizain C <alizain.charolia@gmail.com>
2022-06-20 02:51:42 -0500

    Completed Lab - Week 2, Added Button Components for Tasks List, no blank tasks, no duplicate tasks, tasks date/timing, checkbox button component to remove tasks.

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,31 +1,38 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import "./About.css";
-import profile_pic from "../assets/profile_pic.jpg";
+import alizain from "../assets/alizain.jpg";
 
 export default class About extends Component {
   render() {
     return (
       <div>
-        {/* <p>Design your About me page </p> */}
-        <div class="split left">
-          <div className="centered">
-            <img
-              className="profile_image"
-              src={profile_pic}
-              alt="Profile Pic"
-            ></img>
-          </div>
-        </div>
-        <div className="split right">
-          <div className="centered">
-            <div className="name_title">Your Name</div>
-            <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
-            </div>
-          </div>
+        <div>
+    <div class="split left">
+    <h1 className="ibmplex">ABOUT ME</h1>
+      <div className="centered">
+        <img
+          className="profile_image"
+          src={alizain}
+          alt="Profile Pic"
+          ></img>
+      </div>
+    </div>
+    <div className="split right">
+      <div className="centered">
+        <div className="alizain">Ali Zain Charolia</div>
+        <div className="brief_description">
+          CS + Math + Business @ University of Houston <br></br>
+          SDE Intern @ <a href="https://pros.com">PROS, Inc.</a> | <a href="https://ibm.com">IBM</a> Accelerate '22 <br></br>
+          Aspiring SDE/SWE | ML/AI Enthusiast <br></br>
+          Avid Learner, Team player, Software Engineer <br></br><br></br>
+          Hobbies: Soccer, Tennis, Hiking, <br></br> Binge-wacthing Netflix, and of course, coding! :)
         </div>
       </div>
-    );
+    </div>
+  </div>
+
+      </div>
+
+    )
   }
 }
